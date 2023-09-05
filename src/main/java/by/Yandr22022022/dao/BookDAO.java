2be26bbd92899d9a6009377799ac37ba04a24375
Yandr <102684712+Yandr2022@users.noreply.github.com>
2023-09-05 12:28:06 +0300

    add methods for assign, release Books & getting Book Reader

--- src/main/java/by/Yandr22022022/dao/BookDAO.java
+++ src/main/java/by/Yandr22022022/dao/BookDAO.java
@@ -8,6 +8,7 @@ import org.springframework.jdbc.core.JdbcTemplate;
 import org.springframework.stereotype.Component;
 
 import java.util.List;
+import java.util.Optional;
 
 @Component
 public class BookDAO {
@@ -31,15 +32,28 @@ public class BookDAO {
 
     public void save(Book book) {
         jdbcTemplate.update("INSERT INTO book (title, author, year) " +
-                "VALUES (?,?,?)", book.getTitle(), book.getAuthor(), book.getYearOfPublication());
+                "VALUES (?,?,?)", book.getTitle(), book.getAuthor(), book.getYear());
     }
 
     public void update(int id, Book updatedBook) {
         jdbcTemplate.update("UPDATE book SET title=?, author=?, year=? WHERE id=?", updatedBook.getTitle()
-                , updatedBook.getAuthor(), updatedBook.getYearOfPublication(), updatedBook.getId());
+                , updatedBook.getAuthor(), updatedBook.getYear(), updatedBook.getId());
     }
 
     public void delete(int id) {
         jdbcTemplate.update("DELETE FROM book WHERE id=?", id);
+    }
+
+    public Optional<Person> getBookReader(int id) {
+        return jdbcTemplate.query("SELECT * from book join person on person.id=book.person.id where book.id=?"
+                , new Object[]{id}, new BeanPropertyRowMapper<>(Person.class)).stream().findAny();
+    }
+
+    public void assign(int id, Person reader) {
+        jdbcTemplate.update("UPDATE book set book.person_id=? where id=?", reader.getId(), id);
+    }
+
+    public void release(int id) {
+        jdbcTemplate.update("UPDATE book set book.person_id=null where id=?", id);
     }
 }
